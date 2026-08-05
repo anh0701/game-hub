164548410f10e91a2bc2d19c7db131c054590a6d
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2026-08-05 22:56:21 +0700

    fix button css

--- src/features/sudoku/components/NumberPad.tsx
+++ src/features/sudoku/components/NumberPad.tsx
@@ -6,30 +6,25 @@ const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export function NumberPad({ onNumberClick }: NumberPadProps) {
     return (
-        <div
-            className="
-                grid grid-cols-3
-                gap-2
-                w-full max-w-56
-                "
-        >
+        <div className="grid grid-cols-3 gap-2 justify-items-center">
             {NUMBERS.map((num) => (
                 <button
                     key={num}
-                    onClick={() => {
-                        // console.log("Button:", num);
-                        onNumberClick(num);
-                    }}
+                    onClick={() => onNumberClick(num)}
                     className="
-                        aspect-square
+                        size-10
                         rounded-md
                         border
+                        border-gray-300
                         bg-white
-                        text-lg font-semibold
-                        hover:bg-gray-100
-                        active:scale-95
+                        text-lg
+                        font-semibold
+                        shadow-sm
                         transition
-"
+                        hover:bg-gray-100
+                        hover:shadow
+                        active:scale-95
+                    "
                 >
                     {num}
                 </button>
